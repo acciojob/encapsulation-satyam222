055172ad9eba83ebd593deceffa15d47f1d30492
tiger <satyam7545065@gmail.com>
2023-10-17 19:57:39 +0530

    Your commit message

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
+
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly  info=new RWOnly();
+        // info.name;//it is private member and have read only access
+        info.setName("satyam");
+        // String ans=
+        System.out.print( info.getName());
+
+
+
+
+    }
+
+
+
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class RWOnly {
+    private String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        this.name=newName;
+
+    }
     
 }
